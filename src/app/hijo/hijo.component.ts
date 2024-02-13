73effb09ea807a6db1f6709ab60122748f583347
Mayra <mayra203@gmail.com>
2024-02-12 22:21:56 -0600

    Created child component

---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.scss']
+})
+export class HijoComponent {
+
+}
