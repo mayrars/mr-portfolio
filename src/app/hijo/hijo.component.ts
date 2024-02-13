ddf3d9af007112cd73233c03acf4d69be622db78
Mayra <mayra203@gmail.com>
2024-02-13 06:43:12 -0600

    Moved countern buttons in child component

--- src/app/hijo/hijo.component.ts
+++ src/app/hijo/hijo.component.ts
@@ -7,11 +7,20 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HijoComponent {
   @Output() mensajeDesdeHijo = new EventEmitter<string>()
+  @Output() incrementarDesdeHijo = new EventEmitter<void>()
+  @Output() decrementarDesdeHijo = new EventEmitter<void>()
   
   mensaje: string= ''
 
   enviarMensaje(){
     this.mensajeDesdeHijo.emit(this.mensaje)
+  }
+
+  incrementar(){
+    this.incrementarDesdeHijo.emit()
+  }
+  decrementar(){
+    this.decrementarDesdeHijo.emit() 
   }
 }
  
