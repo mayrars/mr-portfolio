ce969736d77a70b0a755553d1a7bfa46c232781e
Mayra <mayra203@gmail.com>
2024-02-13 06:48:33 -0600

    Created brother component

---
+++ src/app/hermano/hermano.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hermano',
+  templateUrl: './hermano.component.html',
+  styleUrls: ['./hermano.component.scss']
+})
+export class HermanoComponent {
+
+}
